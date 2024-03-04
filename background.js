ce31376e0886142cacbb49d647cd237246decfaa
Ian Kruger <107899965+ianckruger@users.noreply.github.com>
2024-03-04 02:00:17 +0000

    committing some initial files

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if ( request.msg === "hello" ) {
+            chrome.tabs.executeScript(null, { code:"update();"});
+            chrome.browserAction.setIcon({path:"typer.png"});
+        }
+    }
+);
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.speed < 230 && request.speed > 10) {
+            let name = "wmpicons/artboard" + (request.speed - 9) + ".png";
+            chrome.browserAction.setIcon({path: name});
+        }
+        else {
+            chrome.browserAction.setIcon({path: "typer.png"});
+        }
+    });
---
+++ contentScript.js
@@ -0,0 +1,76 @@
+let typing = "";
+let count = 0;
+let shortElapsed = 0;
+let cutTime = 0;
+
+window.addEventListener('keydown', myInput, true);
+
+let shortStart2 = 0;
+
+let elapsed = 0;
+let start = 0;
+let finaltime = 0;
+let wpm = 0;
+
+let check = "";
+
+
+function update(){
+    typing = "";
+    cutTime = 0;
+    wpm = 0;
+    chrome.storage.local.set({'time':wpm});
+}
+
+document.addEventListener("visibilitychange", function () {
+    if(!document.hidden) {
+        chrome.storage.local.set({'time':wpm});
+        chrome.runtime.sendMessage({
+            speed: wpm,
+        });
+    }
+}, false);
+
+window.addEventListener('load', (event) => {
+    if(!document.hidden) {
+        chrome.storage.local.set({'time':wpm});
+        chrome.runtime.sendMessage({
+            speed: wpm
+        });
+    }
+});
+
+function myInput(event){
+    var input = event.which || event.keyCode;
+    if(input != 13 && input != 17 && input != 18){
+        if(typing.length == 0) {
+            masterStart = window.performance.now();
+            start = window.performance.now();
+        }
+        end = window.performance.now;
+        elapsed = end - start;
+        start = window.performance.now();
+
+        if ( elapsed > 1000) {
+            cutTime += elapsed;
+        }
+
+        if(input != 8){
+            let typed = String.fromCharCode(input).toLowerCase();
+            typing = typing + typed;
+        }
+        else {
+            typing = typing.slice(0, typing.length-1);
+        }
+
+        masterElapsed = window.performance.now() - masterStart;
+        finaltime = masterElapsed - cutTime;
+        wpm = Math.round((typing.length/(finaltime/1000/60))/5);
+
+        chrome.runtime.sendMessage({
+            speed: wpm
+        });
+
+        chrome.storage.local.set({'time':wpm});
+    }
+}
