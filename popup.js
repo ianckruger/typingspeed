6b8a883cdc135e5eccb956c77dafa4eacbd28740
Ian Kruger <107899965+ianckruger@users.noreply.github.com>
2024-03-04 02:13:03 +0000

    creating popup

---
+++ popup.js
@@ -0,0 +1,45 @@
+let typing = "";
+let time = 0;
+
+function reset() {
+    typing = "";
+    time = 0;
+
+    chrome.storage.local.set({'reset':"yes"});
+    chrome.tabs.query({'active':true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        if(url == "chrome://newtab/"){
+            document.getElementById("wpm").innerHTML = "Unavailable";
+        }
+        else{
+            document.getElementById(wpm).innerHTML = 0 + " wpm";
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('link');
+
+    link.addEventListener('click', function() {
+        reset();
+        chrome.runtime.sendMessage({
+            msg: "hello"
+        });
+    });
+});
+
+function updatePopup(){
+    chrome.storage.local.get('time', function(data) {
+        time = data.time;
+        document.getElementById("wpm").innerHTML = data.time + " wpm";
+    });
+    chrome.tabs.query({'active':true, 'lastFocusedWindow':true}, function (tabs) {
+        var url = tabs[0].url;
+        if(url == "chrome://newtab/"){
+            document.getElementById("wpm").innerHTML = "Unavailable";
+            chrome.browserAction.setIcon({path: "typer.png"});
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', updatePopup);
